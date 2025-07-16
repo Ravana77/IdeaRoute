94fe1b607548c05edd2ced046b8dc151e3333d7c
Ravana77 <chillehasindu123@gmail.com>
2025-07-16 15:32:33 +0530

    Add initial Login component files

--- IdeaRoute/src/main.jsx
+++ IdeaRoute/src/main.jsx
@@ -1,7 +1,31 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState, useEffect, use } from 'react'
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter, Routers, Route, Navigate, useLocation} from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+
+function RequireAuth({ children, isAuthenticated }) //if u are logged in sends u to home page and if u are not sends u to login page
+ {
+  const location = useLocation();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+ }
+
+function RouterWrapper() //if user is authenticated , even he reload the login authentication will not be lost
+{
+  const [isAuthenticated, setIsAuthentication] = useState
+  (()=> localStorage.getItem('isAuthenticated') === 'true');
+
+}
+
+useEffect(() =>
+{
+  localStorage.getItem('is authenticated', isAuthenticated);
+}, [isAuthenticated]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
