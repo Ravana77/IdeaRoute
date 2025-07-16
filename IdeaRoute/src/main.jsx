1a018d1dfb6f676ab57f3f674f2888fff3441e17
Ravana77 <chillehasindu123@gmail.com>
2025-07-16 20:07:46 +0530

    Refactor routing structure to include authentication and layout components

--- IdeaRoute/src/main.jsx
+++ IdeaRoute/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import {BrowserRouter, Routers, Route, Navigate, useLocation} from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import Layout from './Layout.jsx'
+import Login from './Login.jsx'
 
 function RequireAuth({ children, isAuthenticated }) //if u are logged in sends u to home page and if u are not sends u to login page
  {
@@ -28,12 +30,16 @@ useEffect(() =>
 }, [isAuthenticated]);
 
 
-<Route path = "/login" element={<Login onLogin={() => setIsAuthenticated(true)} />} /> //when someone login its called onLogin() where isAuthenticated is set to true
-
-
+<><Route path="/login" element={<Login onLogin={() => setIsAuthenticated(true)} />} /* when someone login its called onLogin() where isAuthenticated is set to true */ /> 
+  <Route path="/app" element={<RequireAuth isAuthenticated={isAuthenticated}>
+    <Layout>
+      <App />
+    </Layout>
+  </RequireAuth>} /><Route path="/" element={<Navigate to="/login" replace />} /* when someone goes to home page it redirects to login page */ /> 
+</> //when someone goes to home page it redirects to login page
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterWrapper  />
   </StrictMode>,
 )
